17920091736c54742fe90853a3a923c4c03bc213
彭智鑫 <pengzhixin@lizhi.fm>
2023-07-28 15:13:54 +0800

    Initial Commit

---
+++ Netflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix
+//
+//  Created by Angus on 2023/7/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
